37785e1dde8596b2be730c615af6106bff1f1ca0
stephanie <sh985@cornell.edu>
2016-03-23 18:17:59 -0400

    ultra-trivial RNN for RNN-learning

--- tf/models.py
+++ tf/models.py
@@ -8,6 +8,9 @@
 # ------------------------------------------
 
 import tensorflow as tf
+from tensorflow.models.rnn import rnn
+# for testing and learning
+from tensorflow.models.rnn.rnn_cell import BasicRNNCell
 
 def trivial(inputs, n_input, n_hidden, n_output, experiment):
     """
@@ -19,6 +22,7 @@ def trivial(inputs, n_input, n_hidden, n_output, experiment):
     weights = tf.Variable(tf.random_normal(shape=(n_input, n_output)), name='weights')
     bias = tf.Variable(tf.random_normal(shape=(n_input, n_output)), name='bias')
     # create a nonsensical but valid (hopefully) cost function
+    # THIS IS ALL TRASH
     if experiment == 'adding':
         # this means x is rank 3, christ almighty
         hidden = tf.matmul(x[:, :n_input, 0], weights) + bias
@@ -29,5 +33,15 @@ def trivial(inputs, n_input, n_hidden, n_output, experiment):
     else:
         raise NotImplementedError
     cost = tf.reduce_max(hidden)
+    accuracy = tf.reduce_min(hidden)
     parameters = [weights, bias]             # list of Tensors
-    return cost, parameters
+    return cost, accuracy, parameters
+
+def simple_RNN(x, n_hidden=20, batch_size=10, seq_length=15):
+    cell = BasicRNNCell(n_hidden)
+    state_0 = cell.zero_state(batch_size, tf.float32)
+    # split up the input so the RNN can accept it...
+    inputs = [tf.squeeze(input_, [1])
+              for input_ in tf.split(1, seq_length, x)]
+    outputs, final_state = rnn.rnn(cell, inputs, initial_state=state_0)
+    return outputs
