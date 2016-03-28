eb745beec1f15670c110aa7104b5df8ad13493c5
stephanie <sh985@cornell.edu>
2016-03-28 13:47:58 -0400

    now supports checkpointing and loading best model at the end: need to modify the trace dictionary to not save 'best parameters'

--- tf/skeleton.py
+++ tf/skeleton.py
@@ -24,12 +24,11 @@ N_TRAIN = int(1e5)
 N_TEST = int(1e4)
 N_VALI = int(1e4)
 
+DO_TEST = False
+
 # EVERYTHING IS FIRE
 
 # need a way of controlling all the experimental options
-# need thing to get predictions from RNN (model-specific, also experiment-ish)
-# need thing to save parameters/model/graph for 'best results'
-#   (will need to reload these values for the testing at the end)
 
 def get_cost(outputs, y, loss_fn='MSE'):
     """
@@ -47,6 +46,7 @@ def get_cost(outputs, y, loss_fn='MSE'):
         # now this object is shape BATCH_SIZE, output_size
         cost = tf.reduce_mean(tf.sub(output, y) ** 2)
     elif loss_fn == 'CE':
+        # TODO: this is returning a list rather than a single value: fix
         #ok, cross-entropy!
         # (there may be more efficient ways to do this)
         cost = tf.zeros([1])
@@ -77,12 +77,15 @@ def update_step(cost, learning_rate=0.01, decay=0.01,
 
 def main(experiment='adding'):
     # === get options === #
-    state_size = 20
     # temporary constants (will be folded into a cfg)
+    # TODO: fix all
+    state_size = 20
     BATCH_SIZE = 10
     NUM_EPOCHS = 5
     NUM_BATCHES = N_TRAIN / BATCH_SIZE
     T = 100
+    best_model_path = 'best_model.ckpt'
+    trace_path = 'eyo'
 
     # === create data === #
     train_data = data.ExperimentData(N_TRAIN, experiment, T)
@@ -102,23 +105,26 @@ def main(experiment='adding'):
         loss_fn = 'CE'
         assert input_size == 10
 
+    # === construct the graph === #
+    # (doesn't actually matter which one we make placeholders out of)
+    x, y = train_data.make_placeholders()
+
+    # === model select === #
+    outputs = models.simple_RNN(x, input_size, state_size, output_size, sequence_length=sequence_length)
+
+    # === ops and things === #
+    cost = get_cost(outputs, y, loss_fn)
+    train_op = update_step(cost)
+
+    # === for checkpointing the model === #
+    saver = tf.train.Saver()
+
+    # === init === #
+    train_cost_trace = []
+    vali_cost_trace = []
+    best_vali_cost = 1e6
     with tf.Session() as session:
-        # === construct the graph === #
-        # (doesn't actually matter which one we make placeholders out of)
-        x, y = train_data.make_placeholders()
-
-        # === model select === #
-        outputs = models.simple_RNN(x, input_size, state_size, output_size, sequence_length=sequence_length)
-
-        # === ops and things === #
-        cost = get_cost(outputs, y, loss_fn)
-        train_op = update_step(cost)
-
-        # === init === #
-        train_cost_trace = []
-        vali_cost_trace = []
-        best_vali_cost = 1e6
-        tf.initialize_all_variables().run()
+        session.run(tf.initialize_all_variables())
         
         # === train loop === #
         for epoch in xrange(NUM_EPOCHS):
@@ -128,15 +134,17 @@ def main(experiment='adding'):
              
                 train_cost, _ = session.run([cost, train_op], {x: batch_x, y: batch_y})
                 train_cost_trace.append(train_cost)
-                #print epoch, '\t', batch_index, '\t', train_cost
+                print epoch, '\t', batch_index, '\t', train_cost
                 if batch_index % 50 == 0:
                     vali_cost = session.run(cost, {x: vali_data.x, y: vali_data.y})
                     vali_cost_trace.append(vali_cost)
-                    print epoch, '\t', batch_index, '\t\tVALI:', vali_cost
                     if vali_cost < best_vali_cost:
                         # save best parameters
                         best_vali_cost = vali_cost
-                    # save all and values
+                        save_path = saver.save(session, best_model_path)
+                        print epoch, '\t', batch_index, '\t*** VALI:', vali_cost, '\t('+save_path+')'
+                    else:
+                        print epoch, '\t', batch_index, '\t    VALI:', vali_cost
 
                     # NOTE: format consistent with theano version
                     # TODO: update alongside plotting script
@@ -150,14 +158,18 @@ def main(experiment='adding'):
                                  'model': None,
                                  'time_steps': T}
 
-                    cPickle.dump(save_vals, file('test_savefile', 'wb'),
+                    cPickle.dump(save_vals, file(trace_path, 'wb'),
                                  cPickle.HIGHEST_PROTOCOL)
 
             # shuffle the data at each epoch
             train_data.shuffle()
 
-        print 'Training completed. Performance of best model (by validation set) on test data:',
-        #test_cost = session.run([cost], {x: test_x, y: test_y, parameters: best_parameters})
+        print 'Training completed.'
+        if DO_TEST:
+            print 'Loading best model from', best_model_path
+            saver.restore(session, best_model_path)
+            test_cost = session.run(cost, {x: test_data.x, y: test_data.y})
+            print 'Performance on test set:', test_cost
 
 #if __name__ == "__main__":
 #    main()
