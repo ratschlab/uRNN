8b339ae1e4519d37391faf4eb3b6c8fb3083ad46
stephanie <sh985@cornell.edu>
2016-04-29 08:38:34 -0400

    now including summary statistics about test losses

--- tf/simple_plot.R
+++ tf/simple_plot.R
@@ -1,17 +1,40 @@
 library(ggplot2)
 
 args<-commandArgs(TRUE)
-
+PLOT_TRAIN<-FALSE
 #d<-args[1]
-d<-3
+d<-5
 
+fname_base<-paste0('output/simple_qr_d', d, '_bn100_nb10000_')
 
-fname<-'output/simple_lie_algebra_d1_bn100_nb10000_train.txt'
-#fname<-paste0('output/simple_d', d, '_bn100_nb10000_vali.txt')
+#fname<-'output/simple_lie_algebra_d1_bn100_nb10000_train.txt'
 
+# --- vali --- #
+fname<-paste0(fname_base, 'vali.txt')
 data<-read.table(fname, header=T)
 data['rep']<-NULL
 
 ggplot(data, aes(x=training_examples, y=loss, colour=experiment, group=experiment, fill=experiment)) +  ggtitle(paste0("validation set loss (d=", d, ")")) + xlab("# training examples seen") + theme_bw() + stat_summary(fun.data = "mean_se", geom = "smooth")  + theme(legend.position="bottom")
-ggsave("test.png")
-#ggsave(gsub(".txt", ".png", fname))
+ggsave(gsub(".txt", ".png", fname))
+
+# --- train --- # (copy pasta)
+if (PLOT_TRAIN){
+    fname<-paste0(fname_base, 'train.txt')
+    data<-read.table(fname, header=T)
+    data['rep']<-NULL
+
+    ggplot(data, aes(x=training_examples, y=loss, colour=experiment, group=experiment, fill=experiment)) +  ggtitle(paste0("training set loss (d=", d, ")")) + xlab("# training examples seen") + theme_bw() + stat_summary(fun.data = "mean_se", geom = "smooth")  + theme(legend.position="bottom")
+    ggsave(gsub(".txt", ".png", fname))
+}
+
+# --- print summary statistics about test --- #
+fname<-paste0(fname_base, 'test.txt')
+data<-read.table(fname, header=T)
+
+means<-aggregate(data$loss, by=list(data$experiment), FUN=mean)
+names(means)<-c("experiment", "mean")
+sems<-aggregate(data$loss, by=list(data$experiment), FUN=function(x) sd(x)/sqrt(length(x)))
+names(sems)<-c("experiment", "standard error")
+
+test_results<-merge(means, sems)
+print(test_results)
