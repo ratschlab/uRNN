bug fixes, removed test on gradients being None (they aren't, thank jesus)

tf/experiment.py
@@ -22,6 +22,7 @@ from models import RNN
 from data import ExperimentData
 # YOLO
 from unitary_np import lie_algebra_element, lie_algebra_basis_element, numgrad_lambda_update
+from scipy.linalg import expm
 # DEYOLO
 #from unitary_np import numgrad_lambda_update
 
@@ -192,7 +193,7 @@ def main(experiment='adding', batch_size=10, state_size=20,
                     
         # save-specific thing: saving lambdas
         lambda_file = open('output/' + identifier + '_lambdas.txt', 'w')
-        lambda_file.write('batch ' + ' '.join(lambda x: 'lambda_' + str(x), xrange(len(lambdas))) + '\n')
+        lambda_file.write('batch ' + ' '.join(map(lambda x: 'lambda_' + str(x), xrange(len(lambdas)))) + '\n')
     else:
         # nothing special here, movin' along...
         train_op = update_step(cost, learning_rate, gradient_clipping)
@@ -216,38 +217,31 @@ def main(experiment='adding', batch_size=10, state_size=20,
                 if model == 'uRNN':
                     # CONTINUE GRADIENT HACKS
                     # TODO: OPTIMISATION, TESTING
-                    # YOLO making sure U is being updated
+                    # YOLO making sure U is being updated part 1
                     U_re_orig, U_im_orig = session.run(U_variables)
-                    pdb.set_trace()
                     # DEYOLO
                     dcost_dU_re, dcost_dU_im = session.run([g_and_v_U[0][0], g_and_v_U[1][0]], {x:batch_x, y:batch_y})
                     U_new_re_array, U_new_im_array, lambdas, dlambdas = numgrad_lambda_update(dcost_dU_re, dcost_dU_im, lambdas)
                     # YOLO checking numerical gradients (EXPENSIVE)
-                    basic_cost =session.run(cost, {x: batch_x, y:batch_y})
-                    L = unitary_np.lie_algebra_element(d, lambdas)
+                    basic_cost = session.run(cost, {x: batch_x, y:batch_y})
+                    L = lie_algebra_element(state_size, lambdas)
                     numerical_dcost_dlambdas = np.zeros_like(lambdas)
+                    EPSILON=1e-6
                     for e in xrange(len(lambdas)):
-                        perturbed_L = L + EPSILON*unitary_np.lie_algebra_basis_element(d, e, complex_out=True)
-                        perturbed_U = exp(perturbed_L)
-                        perturbed_U_re = tf.constant(np.real(perturbed_U))
-                        perturbed_U_im = tf.constant(np.imag(perturbed_U))
-                        pertubed_cost = session.run(cost, {x: batch_x, y: batch_y, U_re_variable: perturbed_U_re, U_im_variable: perturbed_U_im})
+                        print 100.0*e/len(lambdas)
+                        perturbed_L = L + EPSILON*lie_algebra_basis_element(state_size, e, complex_out=True)
+                        perturbed_U = expm(perturbed_L)
+                        perturbed_U_re = np.real(perturbed_U)
+                        perturbed_U_im = np.imag(perturbed_U)
+                        perturbed_cost = session.run(cost, {x: batch_x, y: batch_y, U_re_variable: perturbed_U_re, U_im_variable: perturbed_U_im})
                         gradient = (perturbed_cost - basic_cost)/EPSILON
+                        print gradient, dlambdas[e]
                         numerical_dcost_dlambdas[e] = gradient
                     # now compare with dlambdas
                     np.mean(dlambdas - numerical_dcost_dlambdas)
                     pdb.set_trace()
                     # DEYOLO
-                    # YOLO making sure gradients aren't all None or anything
-                    nonU_grads = tf.gradients(cost, nonU_variables)
-                    U_grads = tf.gradients(cost, U_variables)
-                    pdb.set_trace()
-                    # DEYOLO
                     train_cost, _, _, _ = session.run([cost, train_op, assign_re_op, assign_im_op], {x: batch_x, y:batch_y, U_new_re: U_new_re_array, U_new_im: U_new_im_array})
-                    # YOLO making sure U is being updated part 2
-                    U_re, U_im = session.run(U_variables)
-                    pdb.set_trace()
-                    # DEYOLO
                 else:
                     train_cost, _ = session.run([cost, train_op], {x: batch_x, y: batch_y})
                 train_cost_trace.append(train_cost)
@@ -282,6 +276,7 @@ def main(experiment='adding', batch_size=10, state_size=20,
 
                 # occasionally, calculate all the gradients
                 if batch_index % 100 == 0:
+                    pass
                     #hidden_states = tf.variables(...) # ... figure out which these are, do I need to name them while defining in RNNCell?
                     #gradz = tf.gradients(cost, hidden_states)
                     #grad_vals = session.run(gradz)