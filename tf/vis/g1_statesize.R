ec3f58a446e4831c5dcc76f3bb72b018e8a28c39
Stephanie Hyland <steph.hyland@gmail.com>
2016-09-20 14:45:20 +0200

    state size analsis graph

--- tf/vis/g1_statesize.R
+++ tf/vis/g1_statesize.R
@@ -2,7 +2,8 @@
 # compare no-bias uRNN at n = 10, 20, 30
 library(ggplot2)
 
-base_dir<-"/home/hyland/git/complex_RNN/tf/output/adding/"
+base_dir<-"/Users/stephanie/PhD/git/complex_RNN/tf/output/adding/"
+#base_dir<-"/home/hyland/git/complex_RNN/tf/output/adding/"
 
 ten<-read.table(paste0(base_dir, 'relu_uRNN_T100_n10.vali.txt'), header=T)
 twenty<-read.table(paste0(base_dir, 'relu_uRNN_T100_n20.vali.txt'), header=T)
@@ -11,9 +12,9 @@ thirty<-read.table(paste0(base_dir, 'relu_uRNN_T100_n30.vali.txt'), header=T)
 batch_size <- 20
 batch_skip <- 150
 
-which <- rep(10, nrow(ten))
-which <- c(which, rep(20, nrow(twenty)))
-which <- c(which, rep(30, nrow(thirty)))
+state_size <- rep(10, nrow(ten))
+state_size <- c(state_size, rep(20, nrow(twenty)))
+state_size <- c(state_size, rep(30, nrow(thirty)))
 num_updates <- seq(nrow(ten))
 num_updates <- c(num_updates, seq(nrow(twenty)))
 num_updates <- c(num_updates, seq(nrow(thirty)))
@@ -22,7 +23,8 @@ num_updates <- batch_skip * num_updates
 da<-rbind(ten, twenty)
 da<-rbind(da, thirty)
 
-da<-data.frame(da, which, num_updates)
+da<-data.frame(da, state_size, num_updates)
+da$state_size <- factor(da$state_size)
 
-ggplot(da, aes(x=num_updates, y=vali_cost)) + geom_point(cex=0.3) + geom_line(alpha=0.2)
-ggsave(paste0(base_dir, "g1_statesize.png"), widht=4.5, height=3)
+ggplot(da, aes(x=num_updates, y=vali_cost, group=state_size, colour=state_size)) + geom_point(cex=0.3) + geom_line(alpha=0.2) + coord_cartesian(ylim=c(0, 0.2)) + geom_hline(yintercept=0.167, linetype='dotted') + xlab("number of batches (batch size: 20)") + ylab("validation set MSE") + ggtitle("state size analysis: uRNN (adding, T=100)")
+ggsave(paste0(base_dir, "g1_statesize.png"), width=5, height=3)
